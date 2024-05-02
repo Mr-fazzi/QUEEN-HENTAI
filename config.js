895130fab6a377cf353c16c760e521d3cfece0ee
Mr-fazzi <168701952+Mr-fazzi@users.noreply.github.com>
2024-05-02 15:35:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+global.owner = [fazi
+  ['03103815563', 'Dinuwa Official²⁰²³', true],
+  ['03103815563', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['94767939688','94767939688'] 
-global.prems = ['94767939688', '94767939688', '94767939688']
+global.mods = ['03103815563','03103815563'] 
+global.prems = ['03103815563', ''03103815563, '03103815563']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
